7e92b4006c77551b97a1c70664a923ec55eb3e03
RomiMolina <roomi.molina@gmail.com>
2024-02-14 21:52:43 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <main className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
-      <header className="text-xl font-bold leading-[3rem]">booscaminas</header>
+      <header className="text-xl font-bold leading-[3rem]">Booscaminas Romi 2.6</header>
       <section className="py-8">👋</section>
       <footer className="text-center leading-[3rem] opacity-70">
         © {new Date().getFullYear()} booscaminas
