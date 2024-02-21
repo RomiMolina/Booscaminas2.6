344917bdf3bca261ed24cbfe9c5d37557e6644f6
RomiMolina <roomi.molina@gmail.com>
2024-02-21 20:05:30 -0300

    timer

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# Ejercicio de entrevista, booscaminas
-
-Hoy es Halloween (o no si venís del futuro) pero las calabazas están de promoción y los sustos están en oferta. Queremos mantener el espiritu de Halloween y para eso vamos a hacer un juego de Buscaminas. No tenemos especificaciones muy claras así que esperamos que nos sorprendas con tu creatividad.
+# Ejercicio booscaminas
 
 ## Requerimientos
 
 - [ ] Debemos ver una grilla de 10x10 casillas donde podemos clickear cada una de ellas.
-- [ ] Al clickear una casilla deberíamos ver en número, cuantas calabazas (bombas) hay alrededor de esa casilla.
-- [ ] Si clickeamos una casilla que tiene una calabaza el juego debería terminar.
+- [ ] Al clickear una casilla deberíamos ver en número, cuantas bombas hay alrededor de esa casilla.
+- [ ] Si clickeamos una casilla que tiene una bomba el juego debería terminar.
 - [ ] Si clickeamos todas las casillas que no tienen calabazas, deberíamos ver un mensaje de victoria.
 - [ ] Al perder debemos emitir un sonido de susto (en la carpeta `public` hay un `.mp3`).
 - [ ] Agregar un botón para reiniciar el juego.
@@ -14,7 +12,7 @@ Hoy es Halloween (o no si venís del futuro) pero las calabazas están de promoc
 ## Extras
 
 - [ ] Podemos elegir el tamaño de la grilla.
-- [ ] Podemos elegir la cantidad de calabazas que hay en la grilla.
+- [ ] Podemos elegir la cantidad de bombas que hay en la grilla.
 - [ ] Podemos poner velas (banderas) en la grilla haciendo click derecho en una casilla.
 - [ ] Podemos ver un contador de casillas que nos quedan por clickear.
 - [ ] Agregar un timer para ver cuánto tiempo tardamos en resolver el juego.
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const GRID_SIZE = 8;
 const MATCHES = [
@@ -37,9 +37,40 @@ for (let rowIndex = 0; rowIndex < MATRIX.length; rowIndex++) {
     MATRIX[rowIndex][cellIndex] = bombCount;
   }
 }
+
+type CellProps = {
+  value: number | string;
+  onClick: () => void;
+  clicked: boolean;
+};
+
+const Cell: React.FC<CellProps> = ({value, onClick, clicked}) => {
+  return (
+    <div
+      className={`h-8 w-8 border flex items-center justify-center ${
+        clicked ? "bg-gray-300" : "bg-gray-500"
+      }`}
+      onClick={onClick}
+    >
+      {clicked ? (value === "B" ? "💣" : value === 0 ? "" : value) : ""}
+    </div>
+  );
+};
+
 function App() {
   const [clicked, setClicked] = useState<string[]>([]);
   const [status, setStatus] = useState<"playing" | "won" | "lost">("playing");
+  const [seconds, setSeconds] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (status === "playing") {
+        setSeconds((prevSeconds) => prevSeconds + 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [status]);
 
   function handleClick(rowIndex: number, cellIndex: number) {
     setClicked((clicked) => clicked.concat(`${rowIndex}-${cellIndex}`));
@@ -53,40 +84,47 @@ function App() {
 
   return (
     <main className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
-      <header className="text-xl font-bold leading-[3rem]">Booscaminas Romi 2.6</header>
-      <section className="flex items-center justify-center flex-col gap-4 text-center">
+      <header className="text-xl font-bold leading-[4rem] flex items-center justify-center mt-8">
+        Booscaminas Romi 2.6
+      </header>
+      <section className="flex items-center justify-center flex-col gap-12 text-center mx-auto ">
+        <p className="text-4xl font-bold text-center">Play now! </p>
+        <div className="text-2xl font-bold">Time: {seconds} seconds</div>
         <section className="py-8">
-          {MATRIX.map((row, rowIndex) => (
-            <article key={String(rowIndex)} className="flex">
-              {row.map((cell, cellIndex) => (
-                <div
+          <div className="grid" id="tablero">
+            {MATRIX.map((row, rowIndex) =>
+              row.map((cell, cellIndex) => (
+                <Cell
                   key={`${rowIndex}-${cellIndex}`}
-                  className={`h-8 w-8 border flex items center justi  fy-center ${clicked.includes(`${rowIndex}-${cellIndex}`) ? "bg-white/20" : "bg-red"}`}
-                >
-                  {clicked.includes(`${rowIndex}-${cellIndex}`) ? (
-                    <span>{cell === "B" ? "❤" : cell === 0 ? null : cell}</span>
-                  ) : (
-                    <button
-                      className="h-full w-full"
-                      type="button"
-                      onClick={() => status === "playing" && handleClick(rowIndex, cellIndex)}
-                    />
-                  )}
-                </div>
-              ))}
-            </article>
-          ))}
+                  clicked={clicked.includes(`${rowIndex}-${cellIndex}`)}
+                  value={cell}
+                  onClick={() => status === "playing" && handleClick(rowIndex, cellIndex)}
+                />
+              )),
+            )}
+          </div>
         </section>
         {status === "won" && (
-          <div>
-            <p>You won</p>
-            <button onClick={() => window.location.reload()}>Play again</button>
+          <div className="text-green-600">
+            <p className="text-2xl font-bold mb-4">You won 🙌</p>
+            <button
+              className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+              onClick={() => window.location.reload()}
+            >
+              Play again
+            </button>
           </div>
         )}
+
         {status === "lost" && (
-          <div>
-            <p>You lost</p>
-            <button onClick={() => window.location.reload()}>Play again</button>
+          <div className="text-red-600">
+            <p className="text-2xl font-bold mb-8">You lost 😢</p>
+            <button
+              className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded"
+              onClick={() => window.location.reload()}
+            >
+              Play again
+            </button>
           </div>
         )}
       </section>
